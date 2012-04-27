ed2022c81fb696a9e706785b2c2fdb6c4c7f3445
Raullen <raullenchai@gmail.com>
2012-04-27 11:15:28 -0700

    support twitter

--- credentials.py
+++
@@ -1,4 +0,0 @@
-Consumer_key = '6Qw2lryncDkrKCijjbDQ'
-Consumer_secret = 'kQyuOjJUbd7rUNjmewUG34R3NFuHrjvEIKy1zAdwY'
-Access_token = '564854472-iYGyfaszp2UYB7wETG5nzVbOy29OZBtGn4ovEMo6'
-Access_token_secret = 'T7Y4DEXPRsGuVE6zHW7V3e1sJJk7Bfyh3MZEqh2gUhU'
